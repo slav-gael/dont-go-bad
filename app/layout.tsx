b360fd65f64e3da49d90ecdc325b4544ae276093
slav <kade.morrill@gmail.com>
2025-11-15 03:21:31 -0700

    Create next app

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+// import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
+
