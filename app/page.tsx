e7ac2f8fff56277436308afc171360091b3c6437
slav <kade.morrill@gmail.com>
2025-11-15 16:18:58 -0700

    Fix page routing

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div>
       Don't Go Bad!
-      <Link href="./About">Cool test link!</Link>
+      <Link href="/about">Cool test link!</Link>
     </div>
   );
 }
