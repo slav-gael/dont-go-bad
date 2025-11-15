6887dda6f66076777365767e091451226bdf872d
Aurora-Spirit <salacamder@gmail.com>
2025-11-15 16:07:02 -0700

    add about page

---
+++ app/About.tsx
@@ -0,0 +1,12 @@
+
+export default function AboutPage() {
+
+
+    return(
+        <>
+            <div>
+                "Hello! This is the About page! Im all about that page amirite hahahahahaha"
+            </div>
+        </>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import Link from 'next/link'
+
+
 export default function Home() {
   return (
     <div>
       Don't Go Bad!
+      <Link href="About.tsx">Cool test link!</Link>
     </div>
   );
 }
