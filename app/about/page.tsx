2b7680347f012c4a624bc78fec1f58f84f60fb90
Aurora-Spirit <salacamder@gmail.com>
2025-11-19 13:08:25 -0700

    about page fix

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,81 +1,90 @@
 import NavBar from "../components/NavBar/NavBar"
 
+import "./About.css"
+
 export default function AboutPage() {
-            <div>
-                <h1>About Don't Go Bad</h1>
-            </div>
-            <div>
-                <h2>
-                    The Mission
-                </h2>
-                <p>
-                    Reducing food waste, saving money, and making cooking easier!
-                </p>
-            </div>
-            <div>
-                <h2>
-                    What is this website?
-                </h2>
-                <p>
-                    Don't Go Bad is a service that helps you use up that 
-                    leftover food you've had in your freezer for a week 
-                    and is about to go bad. We provide an easy way to search 
-                    for recipes that use a specific ingredient.
-                </p>
-            </div>
-            <div>
-                <h2>
-                    How does it work?
-                </h2>
-                <p>
-                    We use TheMealDB API to automatically look up recipes 
-                    from an extensive database, giving you a wide variety 
-                    of options to choose from. Simply type the name of the 
-                    ingredient into the search bar on the home page to get started!
-                </p>
-            </div>
-            <div>
-                <h2>
-                    Who is this site for?
-                </h2>
-                <p>
-                    Busy people, students, families, people who forget what's in their fridge
-                </p>
-            </div>
-            <div>
-                <h2>
-                    Benefits
-                </h2>
-                <p>
-                    Why it's useful:
-                    * saves money
-                    * reduces food waste
-                    * helps with meal planning
-                    * quick recipe discover
-                </p>
-            </div>
-            <div>
-                <h2>
-                    Our Story
-                </h2>
-                <p>
-                    We have forgotten kale one too many times in the back of the fridge to not think about how to fix this issue.
-                </p>
-            </div>
-            <div>
-                <h2>
-                    Future Features
-                </h2>
-                <p>
-                    ?
-                </p>
-            </div>
-            <div>
-                <h2>
-                    Current Limitations
-                </h2>
-                <p>
-                    Our database depends upon TheMeanDB, so our recipes are limited.
-                </p>
-            </div>
+    return (
+        
+    <>
+        <NavBar />
+        <div>
+            <h1>About Don't Go Bad</h1>
+        </div>
+        <div className="textBlurb">
+            <h2>
+                The Mission
+            </h2>
+            <p>
+                Reducing food waste, saving money, and making cooking easier!
+            </p>
+        </div>
+        <div className="textBlurb">
+            <h2>
+                What is this website?
+            </h2>
+            <p>
+                Don't Go Bad is a service that helps you use up that 
+                leftover food you've had in your freezer for a week 
+                and is about to go bad. We provide an easy way to search 
+                for recipes that use a specific ingredient.
+            </p>
+        </div>
+        <div className="textBlurb">
+            <h2>
+                How does it work?
+            </h2>
+            <p>
+                We use TheMealDB API to automatically look up recipes 
+                from an extensive database, giving you a wide variety 
+                of options to choose from. Simply type the name of the 
+                ingredient into the search bar on the home page to get started!
+            </p>
+        </div>
+        <div className="textBlurb">
+            <h2>
+                Who is this site for?
+            </h2>
+            <p>
+                Busy people, students, families, people who forget what's in their fridge
+            </p>
+        </div>
+        <div className="textBlurb">
+            <h2>
+                Benefits
+            </h2>
+            <p>
+                Why it's useful:
+                * saves money
+                * reduces food waste
+                * helps with meal planning
+                * quick recipe discover
+            </p>
+        </div>
+        <div className="textBlurb">
+            <h2>
+                Our Story
+            </h2>
+            <p>
+                We have forgotten kale one too many times in the back of the fridge to not think about how to fix this issue.
+            </p>
+        </div>
+        <div className="textBlurb">
+            <h2>
+                Future Features
+            </h2>
+            <p>
+                ?
+            </p>
+        </div>
+        <div className="textBlurb">
+            <h2>
+                Current Limitations
+            </h2>
+            <p>
+                Our database depends upon TheMeanDB, so our recipes are limited.
+            </p>
+        </div>
+    </>
+    )
+            
         }
