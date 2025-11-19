cc53f650c351443cd03ca67890241e201e777001
Aurora-Spirit <salacamder@gmail.com>
2025-11-19 12:26:39 -0700

    fixed up a little css on the about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,4 +1,5 @@
 import NavBar from "../components/NavBar/NavBar"
+import "./About.css"
 
 export default function AboutPage() {
 
@@ -9,14 +10,14 @@ export default function AboutPage() {
             <div>
                 <h1>About Don't Go Bad</h1>
             </div>
-            <div>
+            <div className="textBlurb">
                 <h2>What is this website?</h2>
                 <p>Don't Go Bad is a service that helps you use up that 
                     leftover food you've had in your freezer for a week 
                     and is about to go bad. We provide an easy way to search 
                     for recipes that use a specific ingredient.</p>
             </div>
-            <div>
+            <div className="textBlurb">
                 <h2>How does it work?</h2>
                 <p>We use TheMealDB API to automatically look up recipes 
                     from an extensive database, giving you a wide variety 
